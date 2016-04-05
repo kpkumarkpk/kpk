e1b8da139477cfa107b9ce33c5447ceae547c0fc
D.Yoganjulu Reddy <outthinkingpvtltd@otpl-138.local>
2016-04-05 19:52:11 +0530

    Initial Commit

---
+++ pipframes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pipframes
+//
+//  Created by OUTTHINKING PVT LTD on 4/5/16.
+//  Copyright © 2016 OUTTHINKING PVT LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pipframes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pipframes
+//
+//  Created by OUTTHINKING PVT LTD on 4/5/16.
+//  Copyright © 2016 OUTTHINKING PVT LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
